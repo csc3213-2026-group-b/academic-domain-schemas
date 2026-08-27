5d2c2b43bd095245ca6ff2a7f56688eaa3c7b131
Sivothayan <hi@sivothajan.dev>
2026-08-27 07:29:17 +0530

    test: guard the package public API

---
+++ tests/contract/public-api.test.ts
@@ -0,0 +1,77 @@
+import { describe, expect, it } from 'bun:test';
+
+import * as publicApi from '../../src/index.js';
+
+const expectedRuntimeExports = [
+  'AcademicPeriodSchema',
+  'AcademicRankSchema',
+  'AcademicSupportPositionSchema',
+  'AcademicSupportStaffSchema',
+  'AcademicTeachingStaffSchema',
+  'AcademicUsernameSchema',
+  'AcademicYearSchema',
+  'AlumniBatchListSchema',
+  'AlumniBatchSchema',
+  'ConferenceSchema',
+  'CourseCodeSchema',
+  'CourseIdSchema',
+  'CourseOfferingIdSchema',
+  'CourseOfferingSchema',
+  'CourseSchema',
+  'CourseStaffSchema',
+  'HonoursStreamSchema',
+  'KnownSocialIcons',
+  'KnownSocialPlatforms',
+  'NonAcademicPositionSchema',
+  'NonAcademicStaffSchema',
+  'PeopleSearchEntrySchema',
+  'PeopleSearchEntryTypeSchema',
+  'PeopleSearchIndexSchema',
+  'PersonSchema',
+  'PostgraduateProgrammeDefinitionListSchema',
+  'PostgraduateProgrammeDefinitionSchema',
+  'PostgraduateProgrammeSchema',
+  'ProgramSchema',
+  'ProjectCourseOfferingSchema',
+  'ProjectCourseSchema',
+  'ProjectDatesSchema',
+  'ProjectLinksSchema',
+  'ProjectMediaSchema',
+  'ProjectPersonRoleSchema',
+  'ProjectPersonSchema',
+  'ProjectSchema',
+  'ProjectSourceSchema',
+  'ProjectStatusSchema',
+  'ProjectTypeSchema',
+  'PublicationSchema',
+  'ResearchSchema',
+  'SNumberSchema',
+  'SlqfLevelSchema',
+  'SocialIconSchema',
+  'SocialLinksSchema',
+  'StaffSchema',
+  'StudentLevelSchema',
+  'StudentPlacementListSchema',
+  'StudentPlacementSchema',
+  'StudentSchema',
+  'StudentStatusSchema',
+  'StudentStreamDefinitionListSchema',
+  'StudentStreamDefinitionSchema',
+  'StudentStreamSchema',
+  'StudentTrackSchema',
+  'StudentTypeSchema',
+  'detectKnownSocialUrl',
+  'normalizeKnownSocialUrl',
+] as const satisfies readonly (keyof typeof publicApi)[];
+
+describe('package public API', () => {
+  it.each(expectedRuntimeExports)('exports %s', (exportName) => {
+    expect(publicApi[exportName]).toBeDefined();
+  });
+
+  it('does not expose an unexpected runtime contract', () => {
+    expect(Object.keys(publicApi).sort()).toEqual(
+      [...expectedRuntimeExports].sort()
+    );
+  });
+});
